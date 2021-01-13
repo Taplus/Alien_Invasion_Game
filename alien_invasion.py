bf68abdd9c7083a5cabc1b43c3150641a21eaa8b
Taplus <coffeelake@live.com>
2021-01-13 22:59:17 +0800

    创建 设置类 ship类

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,16 @@
 import sys
 import pygame
+from  setting import Settings
 
 def run_game():
+    ai_settings = Settings()#创建一个实例
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
-    pygame.display.set_caption("Alien Invasion")
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("外星人入侵")
 
+    #设置背景色
+    bg_color = (ai_settings.bg_color)#灰色
     #开始游戏的主循环
     while True:
 
@@ -14,6 +18,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        #用背景色填充屏幕
+        screen.fill(bg_color)
 
         #让最近绘制的屏幕可见
         pygame.display.flip()
