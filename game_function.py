21afff354c6a651434f9c3ed37625ea5875eb78a
Taplus <coffeelake@live.com>
2021-01-14 20:55:38 +0800

    整群外星人

--- game_function.py
+++ game_function.py
@@ -46,22 +46,24 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-def create_fleet(ai_settings,screen,aliens):
+def create_fleet(ai_settings,screen,ship,aliens):
     '''创建外星人群'''
     alien = Alien(ai_settings,screen)
     number_aliens_x = get_number_aliens_x(ai_settings,alien.rect.width)
+    number_rows = get_number_rows(ai_settings,ship.rect.height,alien.rect.height)
+    for row_number in range(number_rows):
+        #创建第一行外星人
+        for alien_number in range(number_aliens_x):
+            #创建一个外星人并将其加入到当前行
+            create_alien(ai_settings,screen,aliens,alien_number,row_number)
 
-    #创建第一行外星人
-    for alien_number in range(number_aliens_x):
-        #创建一个外星人并将其加入到当前行
-        create_alien(ai_settings,screen,aliens,alien_number)
-
-def create_alien(ai_settings,screen,aliens,alien_number):
+def create_alien(ai_settings,screen,aliens,alien_number,row_number):
     '''创建一个外星人并将其放在当前行'''
     alien = Alien(ai_settings,screen)
     alien_width = alien.rect.width
     alien.x = alien_width + 2 * alien_width * alien_number
     alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
     aliens.add(alien)
 
 def get_number_aliens_x(ai_settings,alien_width):
@@ -69,6 +71,12 @@ def get_number_aliens_x(ai_settings,alien_width):
     available_space_x = ai_settings.screen_width - 2 * alien_width
     number_aliens_x = int(available_space_x / (2 * alien_width))
     return number_aliens_x
+
+def get_number_rows(ai_settings,ship_height,alien_height):
+    '''计算屏幕可容纳多少行外星人'''
+    available_space_y = (ai_settings.screen_height - (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
 
 def update_screen(ai_settings,screen,ship,aliens,bullets):
     '''更新屏幕上的图像并切换到新屏幕'''
