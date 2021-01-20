cd4b58e91ff609392024a476c1615732ded853cc
Taplus <coffeelake@live.com>
2021-01-20 20:48:55 +0800

    可玩状态

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from ship import Ship
 from alien import Alien
 import game_function as gf
 from pygame.sprite import Group
+from game_stats import GameStats
 
 def run_game():
     
@@ -27,12 +28,18 @@ def run_game():
     aliens = Group()
     gf.create_fleet(ai_settings,screen,ship,aliens)
 
+    #创建一个用于存储游戏信息的实例
+    stats = GameStats(ai_settings)
+
     #开始游戏的主循环
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        gf.update_bullets(aliens,bullets)
-        gf.update_aliens(ai_settings,aliens)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+        
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
